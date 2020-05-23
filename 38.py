eedfca403a87746c9ed8e08a5c6874b27f271a4f
Peng.Zhou <18811746117@163.com>
2020-05-23 20:46:10 +0800

    add 38.py

--- 38.py
+++ 38.py
@@ -1,22 +1,23 @@
 from typing import List
 class Solution:
-    def permutation(self, s: str) -> List[str]:
-        def dfs(s,start,temp):
-            if len(temp)==len(s):
-                res.append(temp.copy())
+    def permute(self, nums):
+        def back(nums,temp):
+            # print("temp的值为:",sets.add(sum(temp)))
+            # res+=temp
+            if len(nums)==0:
+                res_all.append(temp)
                 return 
-            for i in range(start,len(s)): 
-                temp.append(s[i])
-                print('debug->')
-                dfs(s,i+1,temp)#i = 0 a b c  i=1 
-                temp.pop()
-                print('????')
-            return 
-        temp,res=[],[]
-        dfs(s,0,temp)
-        return res
-nums='abc'
-sol =Solution()
-res = sol.permutation(nums)
-print(res)
+            for i in range(len(nums)):
+                back(nums[:i]+nums[i+1:],temp+[nums[i]]) #如何记录上一次的数组状态？ 
+                #将上层的值放在参数中，这样就可以保留上次递归传入的参数值
+        res_all=[]
+        temp=[]
+        # sets=set()
+        back(nums,temp)
+        # print(sets)
+        return res_all
         
+s = [1,3]       
+sol = Solution()
+res = sol.permute(s)
+print(res)
--- test.py
+++ test.py
@@ -1,3 +1,25 @@
-a=1
-b=0
-c = a|b
+from typing import List
+class Solution:
+    def permutation(self, s: str) -> List[str]:
+        def back(nums,temp):
+            # print("temp的值为:",sets.add(sum(temp)))
+            # res+=temp
+            if len(nums)==0:
+                sets.add(temp)
+                res_all.append(temp)
+                return 
+            for i in range(len(nums)):
+                # print(temp+[nums[i]])
+                back(nums[:i]+nums[i+1:],temp+nums[i]) #如何记录上一次的数组状态？ 
+                #将上层的值放在参数中，这样就可以保留上次递归传入的参数值
+        res_all=[]
+        temp=''
+        sets=set()
+        back(s,temp)
+        # print(sets)
+        
+        return list(sets)
+s='aabaaba'
+sol = Solution()
+res =sol.permutation(s)
+print(res)
