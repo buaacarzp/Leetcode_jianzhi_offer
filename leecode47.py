1f972edbfa14bd0af9c428a5150cb3db4478f35e
Peng.Zhou <18811746117@163.com>
2020-05-23 21:41:57 +0800

    add leetcode47.py

---
+++ leecode47.py
@@ -0,0 +1,24 @@
+'''
+list在哈希中不能去重，所以该如何写
+'''
+from typing import List
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        def back_track(nums,temp):
+            if len(nums)==0:
+                res_all.append(temp)
+            for i in range(len(nums)):
+                if  i>0 and nums[i]==nums[i-1]:
+                    continue
+                else:
+                    back_track(nums[:i]+nums[i+1:],temp+[nums[i]])
+                    
+                
+        temp=[]
+        res_all=[]
+        back_track(nums,temp)
+        return res_all
+nums =[1,1,2,2]
+sol = Solution()
+res =sol.permuteUnique(nums)
+print(res)
